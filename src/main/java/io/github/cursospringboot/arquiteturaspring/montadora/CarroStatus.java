25483287eae7dfb3f3c26edb5c36cfe495b25813
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-01-02 17:38:21 -0300

    Criando uma Configuration e registrando um Bean

---
+++ src/main/java/io/github/cursospringboot/arquiteturaspring/montadora/CarroStatus.java
@@ -0,0 +1,4 @@
+package io.github.cursospringboot.arquiteturaspring.montadora;
+
+public record CarroStatus(String mensagem) {
+}
---
+++ src/main/java/io/github/cursospringboot/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -0,0 +1,23 @@
+package io.github.cursospringboot.arquiteturaspring.montadora.api;
+
+import io.github.cursospringboot.arquiteturaspring.montadora.CarroStatus;
+import io.github.cursospringboot.arquiteturaspring.montadora.Chave;
+import io.github.cursospringboot.arquiteturaspring.montadora.HondaHRV;
+import io.github.cursospringboot.arquiteturaspring.montadora.Motor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteFabricaController {
+
+    @Autowired
+    private Motor motor;
+
+    @PostMapping
+    public CarroStatus ligarCarro(@RequestBody Chave chave) {
+        var carro = new HondaHRV(motor);
+        return carro.darIgnicao(chave);
+    }
+}
