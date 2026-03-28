37b5002346053160d30b04c9a4ee2d6beabc047a
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-03-28 14:48:59 -0300

    Aprendendo sobre Escopo dos beans @Scope

--- src/main/java/io/github/cursospringboot/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/io/github/cursospringboot/arquiteturaspring/BeanGerenciado.java
@@ -3,10 +3,29 @@ package io.github.cursospringboot.arquiteturaspring;
 import io.github.cursospringboot.arquiteturaspring.todos.TodoEntity;
 import io.github.cursospringboot.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+// singleton: Escopo padrão de qualquer bean
+// singleton -> Padrão de projeto em que eu tenho uma instância única de um objeto que vai atender a toda a aplicação.
+// prototype -> Padrão de projeto contrário ao singleton, para cada requisição, usuário, ele vai criar uma instância.
+
+//@Scope("singleton")
+//@Scope("request") -> Aplicação Web: Ao mandar uma requisição, o objeto so vai existir durante esta requisição, e depois o objeto vai morrer.
+//@Scope("session") -> Aplicação Web: Objeto que vai durar, enquanto a sessão do usuário durar, interessante para guardar dados do usuário que esta logado, por exemplo. -> guarda estado
+//@Scope("application") -> Aplicação Web: Parece com o session, mas se extende a todos os usuários. -> guarda estado
+
+/** Ao invés de digitar dentro da annotation */
+// @Scope(WebApplicationContext.SCOPE_REQUEST)
+// @Scope(WebApplicationContext.SCOPE_SESSION)
+// @Scope(WebApplicationContext.SCOPE_APPLICATION)
+
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class BeanGerenciado {
+
+    private String idUsuarioLogado;
 
     @Autowired /** Injeção via propriedade */
     private TodoValidator validator;
